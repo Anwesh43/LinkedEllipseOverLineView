0929e76d35d7a851827fe9bc3c5cded8bf461fb7
Anwesh43 <anweshthecool0@gmail.com>
2019-04-07 11:32:48 +0530

    adding EOLNode class

--- ellipseoverlineview/src/main/java/com/anwesh/uiprojects/ellipseoverlineview/EllipseOverLineView.kt
+++ ellipseoverlineview/src/main/java/com/anwesh/uiprojects/ellipseoverlineview/EllipseOverLineView.kt
@@ -129,4 +129,48 @@ class EllipseOverLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class EOLNode(var i : Int, val state : State = State()) {
+
+        private var next : EOLNode? = null
+        private var prev : EOLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = EOLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawEOLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : EOLNode {
+            var curr : EOLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
