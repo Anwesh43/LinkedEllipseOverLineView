0d69f53cf6f0a0b21e815ebb44390097b776eaff
Anwesh43 <anweshthecool0@gmail.com>
2019-04-07 11:01:37 +0530

    adding libraries and contants

--- ellipseoverlineview/src/main/java/com/anwesh/uiprojects/ellipseoverlineview/EllipseOverLineView.kt
+++ ellipseoverlineview/src/main/java/com/anwesh/uiprojects/ellipseoverlineview/EllipseOverLineView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.ellipseoverlineview
 /**
  * Created by anweshmishra on 07/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#f44336")
+val backColor : Int = Color.parseColor("#BDBDBD")
