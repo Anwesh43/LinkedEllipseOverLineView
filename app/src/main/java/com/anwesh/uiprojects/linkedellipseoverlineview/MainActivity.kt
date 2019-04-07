1a78f4aefe064293eab5d77931beb8411d430fd7
Anwesh43 <anweshthecool0@gmail.com>
2019-04-07 11:43:02 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedellipseoverlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedellipseoverlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedellipseoverlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.ellipseoverlineview.EllipseOverLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        EllipseOverLineView.create(this)
     }
 }
